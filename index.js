a6ded90f034e2cca6fe5be49bc04f884c301699e
asabya <asabya@gmail.com>
2023-03-11 15:00:35 +0530

    prepare

--- index.js
+++ index.js
@@ -7,21 +7,11 @@ async function setup() {
   try {
     // Get version of tool to be installed
     const version = core.getInput('version');
-    console.log(`setup Version: ${ version }`)
-    // Download the specific version of the tool, e.g. as a tarball/zipball
-    const download = getDownloadObject(version);
-    console.log(`setup Download url: ${ download.url }`)
-    console.log(`setup Download binPath: ${ download.binPath }`)
-    const pathToTarball = await tc.downloadTool(download.url);
-    console.log(`setup pathToTarball: ${ pathToTarball }`)
+    const downloadURL = getDownloadObject(version);
+    const pathToTarball = await tc.downloadTool(downloadURL);
 
-    // Extract the tarball/zipball onto host runner
     const pathToCLI = await tc.extractZip(pathToTarball);
-    console.log(`setup pathToC  LI: ${ pathToCLI }`)
-    // Expose the tool by adding it to the PATH
     core.addPath(pathToCLI);
-    core.addPath(path.join(pathToCLI, download.binPath));
-    console.log(`setup path.join(pathToCLI, download.binPath): ${ path.join(pathToCLI, download.binPath) }`)
   } catch (e) {
     core.setFailed(e);
   }
