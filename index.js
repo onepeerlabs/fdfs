41c15d8d3daceac3b5c328a1c3f7e048f1cb1bc5
asabya <asabya@gmail.com>
2023-03-18 16:50:47 +0530

    add upload logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 
-const { getDownloadObject, startDfs, wait } = require('./lib/utils');
+const { getDownloadObject, startDfs, wait, userLogin, move } = require('./lib/utils');
 
 async function setup() {
   try {
@@ -15,12 +15,44 @@ async function setup() {
     const bee = process.env.BEE
     const rpc = process.env.RPC
     const stamp = process.env.STAMP
+    const username = process.env.USER_NAME
+    const password = process.env.PASSWORD
+    const pod = process.env.POD
+    let destination = process.env.ROOT || "/"
+    if (bee == null || bee === "") {
+      core.setFailed("Environment variable BEE is not set")
+      return
+    }
+    if (rpc == null || rpc === "") {
+      core.setFailed("Environment variable RPC is not set")
+      return
+    }
+    if (stamp == null || stamp === "") {
+      core.setFailed("Environment variable STAMP is not set")
+      return
+    }
+    if (username == null || username === "") {
+      core.setFailed("Environment variable USER_NAME is not set")
+      return
+    }
+    if (password == null || password === "") {
+    core.setFailed("Environment variable PASSWORD is not set")
+      return
+    }
+    if (pod == null || pod === "") {
+      core.setFailed("Environment variable POD is not set")
+      return
+    }
+    if (destination === "") {
+        destination = "/"
+    }
 
-    console.log(`Bee: ${bee}`);
-    console.log(`RPC: ${rpc}`);
-    console.log(`STAMP: ${stamp}`);
     const dfsProcess = await startDfs(bee, rpc, stamp);
     await wait();
+    await userLogin(username, password);
+    const sourcePath = core.getInput('path', {required: true});
+
+    await move(pod, sourcePath, destination);
     dfsProcess.unref();
     process.exit();
   } catch (e) {
@@ -31,5 +63,5 @@ async function setup() {
 module.exports = setup
 
 if (require.main === module) {
-  setup();
+  setup().then(r => console.log(r));
 }
